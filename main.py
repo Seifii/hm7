19ff8d7689a7cec48faabb21b314f9055a4b3125
Seifii <sofiapetrova893@gmail.com>
2025-05-24 14:26:40 +0300

    1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 class n:
     def __init__(self):
-        self.data = [10, 20, 30]
+        self.m = [10, 20, 30]
 
     def __iter__(self):
-        return iter(self.data)
+        return iter(self.m)
